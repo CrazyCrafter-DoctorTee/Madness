9a6f7039ad303b90fd31eb0628d459e50630f401
DoctorTee <crazycrafter1@gmail.com>
2018-07-10 22:35:02 -0500

    Final matching to master

--- player.py
+++ player.py
@@ -8,8 +8,8 @@ class Player:
         self.maxRow = maxRow
         self.maxCol = maxCol
         self.row = 0
-        self.col = 0        
-    
+        self.col = 0
+
     def move(self, key):
         if key == pygame.K_LEFT:
             self.col -= 1 if self.col != 0 else 0
@@ -19,6 +19,6 @@ class Player:
             self.row += 1 if self.row < self.maxRow - 1 else 0
         if key == pygame.K_UP:
             self.row -= 1 if self.row != 0 else 0
-            
+
     def draw(self, screen):
         load_image(screen, self.image, self.row, self.col)
