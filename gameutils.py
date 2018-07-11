673a2062f4c7ca7dd9d8a0f83424437c446973e3
DoctorTee <crazycrafter1@gmail.com>
2018-07-10 22:09:25 -0500

    updated gamemanager to match master

--- gameutils.py
+++ gameutils.py
@@ -1,26 +1,4 @@
 import pygame
-import configparser
-
-import gamemap
-
-def init_madness():
-    images = {}
-    maps = {}
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 704))
-    config = configparser.ConfigParser()
-    config.read('assets/game.cfg')
-    for key in config['images']:
-        print('Image:', key)
-        images[key] = pygame.image.load(config['images'][key])
-
-    for key in config['maps']:
-        print('Map:', key)
-        mapname = config['maps'][key]
-        print(mapname)
-        maps[key] = gamemap.GameMap(mapname, images)
-
-    return screen, images, maps
 
 def load_image(screen, image, row, col, scale=True):
     if scale:
