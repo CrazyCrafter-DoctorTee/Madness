83dc9f2d2a78f06470ba67d9eaa7175a50fae909
DoctorTee <crazycrafter1@gmail.com>
2018-07-14 22:10:53 -0500

    mo critter

--- critter.py
+++ critter.py
@@ -1,15 +1,14 @@
-import IOManager
+import ioman
 
 class Critter:
-    def __init__(self, name, iomanager, lvl=1, currentmoves=None, currenthp=None):
-        statsin = iomanager.get_data('critters', name, 'stats')
-        self.moves = iomanager.get_data('critters', name, 'moves')
-        self.iomanager = iomanager
-        self.def = statsin['def']
+    def __init__(self, name, ioman, lvl=1, currentmoves=None, currenthp=None):
+        self.ioman = ioman
+        statsin = self.ioman.get_data('critters', name, 'stats')
+        self.moves = self.ioman.get_data('critters', name, 'moves')
+        self.defense = statsin['def']
         self.hp = statsin['hp']
         self.spd = statsin['spd']
         self.atk = statsin['atk']
-        self.exp = exp
         self.lvl = lvl
         self.status = []
         if(currenthp == None or currenthp > self.hp):
@@ -20,7 +19,7 @@ class Critter:
             self.currentmoves = []
             i = self.lvl
             while len(self.currentmoves) < 4 and i > 0:
-                for j in range(len(bestmoves), 4):
+                for j in range(len(self.currentmoves), 4):
                     for key, value in self.moves.items():
                         if(value == i):
                             self.currentmoves.append(key)
@@ -29,12 +28,15 @@ class Critter:
             self.currentmoves = currentmoves
 
     def defend(self, attack):
+        self.hp -= int(attack[0] / self.defense)
+        if(len(attack) > 1):
+            print("handling status effects")
 
     def attack(self, move):
         if self.currenthp == 0:
             return (0)
         else:
-            movedict = self.iomanager.get_data('moves', move)
+            movedict = self.ioman.get_data('moves', move)
             if 'status' in movedict:
                 return (movedict['dmg'] * self.atk, movedict['status'])
             else:
