ef479adbbb856ba68517bd6acd640ae6950b7f05
DoctorTee <crazycrafter1@gmail.com>
2018-07-14 19:40:32 -0500

    fix changing screen dims

--- mapstate.py
+++ mapstate.py
@@ -8,24 +8,22 @@ import gamemap
 
 
 class MapState(gamestate.GameState):
-    
+
     def __init__(self, ioManager, screen):
         self.ioManager = ioManager
         self.screen = screen
-        self.screenDims = self.ioManager.get_data('game', 'graphics', 'screendims')
-        self.init()
-        self.player = player.Player(self.currMap, self.ioManager.get_data('game', 'map', 'startdims'))
-        self.enemy = enemy.Enemy(self.images['character']['enemy'], self.maps['start'], self.screenDims)
-        self.camera = camera.Camera(self.screenDims, self.maps['start'].maxDims)
-        
-    def init(self):
+        self.screenDims = pygame.display.get_surface().get_size()
         self.images = self.create_images(self.ioManager.get_data('images'))
         maps = self.ioManager.get_data('maps')
         self.maps = {}
         for name, attribs in maps.items():
             self.maps[name] = gamemap.GameMap(attribs['filename'], attribs['tiledims'])
         self.currMap = self.maps[self.ioManager.get_data('game', 'map', 'startmap')]
-    
+        self.player = player.Player(self.currMap, self.ioManager.get_data('game', 'map', 'startdims'))
+        self.enemy = enemy.Enemy(self.images['character']['enemy'], self.maps['start'], [320, 320])
+        self.camera = camera.Camera(self.screenDims, self.maps['start'].maxDims)
+
+
     def process_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,7 +33,7 @@ class MapState(gamestate.GameState):
             if event.type == pygame.KEYUP:
                 self.player.key_up(event.key)
         return True
-                
+
     def draw_map(self, tileList, startCords, tileSize):
         i, x = 0, startCords[0]
         while i < len(tileList):
@@ -45,7 +43,7 @@ class MapState(gamestate.GameState):
                 self.load_image(tile, (x, y))
                 j, y = j+1, y+tileSize[1]
             i, x = i+1, x+tileSize[0]
-            
+
     def make_actions(self):
         self.player.move()
         self.enemy.move()
@@ -58,4 +56,3 @@ class MapState(gamestate.GameState):
         self.load_image(self.images['character']['enemy'], self.enemy.cords)
         self.load_image(self.images['character']['player'], playerCords)
         pygame.display.flip()
-        
