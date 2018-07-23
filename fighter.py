03f48ef1eb7cf65f0ddd89b1895653cc27437a2b
DoctorTee <crazycrafter1@gmail.com>
2018-07-23 09:13:29 -0500

    cleanup

--- fighter.py
+++
@@ -1,7 +0,0 @@
-import critter
-
-class Fighter(object):
-    
-    def __init__(self, iomanager):
-        self.iomanager = iomanager
-        self.critters = [critter.Critter('doge', iomanager, 5), critter.Critter('doge', iomanager, 5)]
